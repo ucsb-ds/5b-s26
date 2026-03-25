54d85bc91aeac66285b2a2260057408827722729
Phill Conrad <pconrad@cs.ucsb.edu>
2026-03-25 16:21:09 -0700

    pc - remove _site and .jekyll-metadata

--- _site/macos_ruby.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-rvm install 3.3.4 --with-openssl-dir=/opt/homebrew/opt/openssl@3.3
--- _site/serve.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rm -rf _site
-jekyll build
-(sleep 3; open http://localhost:4000) & 
-jekyll serve
